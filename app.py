231328a27f924ae4f229141d5bb617f80336ecac
Veda725 <228x1a42c8@khitguntur.ac.in>
2025-07-04 21:52:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 import hashlib
 import secrets
 
-
+app = Flask(__name__)
 app.secret_key = '2f9b3e7d56d749cdab6f9cf672d2a937c64f0f19c4eaefc17264f2d39a5314bb'
 
 # AWS Configuration
@@ -140,7 +140,7 @@ def login_required(f):
             flash('Login required', 'error')
             return redirect(url_for('login'))
         return f(*args, **kwargs)
-    wrapper.__name__ = f.__name__
+    wrapper._name_ = f._name_
     return wrapper
 
 @app.route('/')
@@ -247,5 +247,5 @@ def confirmation(booking_id):
 def admin():
     return render_template('admin.html', movies=get_movies())
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True, host='0.0.0.0')
